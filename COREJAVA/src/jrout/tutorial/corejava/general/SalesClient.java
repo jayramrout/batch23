e9dbfc5d3b54ada3ad1e912a33389db2953b1b87
jayramrout <jayram.rout@hotmail.com>
2016-09-25 20:59:02 -0400

    Update

--- COREJAVA/src/jrout/tutorial/corejava/general/SalesClient.java
+++ COREJAVA/src/jrout/tutorial/corejava/general/SalesClient.java
@@ -1,12 +1,16 @@
 package jrout.tutorial.corejava.general;
 
-import jrout.tutorial.calculator.JayramCalculator;
+//import jrout.tutorial.calculator.JayramCalculator;
 
-
+/**
+ * This was part of the example where we saw how to import external JAR file
+ * @author jrout
+ *
+ */
 public class SalesClient {
 
 	public static void main(String[] args) {
-		System.out.println(JayramCalculator.jayramAdd(2, 3));
+//		System.out.println(JayramCalculator.jayramAdd(2, 3));
 		
 	}
 
